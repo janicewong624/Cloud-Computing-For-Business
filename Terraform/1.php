1e07e2a365ebcaacca6d7129242510784448813f
janicewong624 <janicewongyukru07@gmail.com>
2026-08-30 20:51:00 +0800

    test14

---
+++ Terraform/1.php
@@ -0,0 +1,11 @@
+<?php
+require 'config.php';
+require 'auth.php';
+
+$pageTitle = 'About';
+require 'partials/header.php';
+?>
+<div class="page-header">
+<h1>Abouts This Platform</h1>
+<p>What Academic &amp; Library Resource Scheduling is, and how it works.</p>
+</div>
