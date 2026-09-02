0c906a1c6fd0588de441252e4f78d4f5646e9cc0
janicewong624 <janicewongyukru07@gmail.com>
2026-09-02 21:06:25 +0800

    test33

--- Terraform/asg.tf
+++ Terraform/asg.tf
@@ -1,38 +1,17 @@
-# IAM Role for EC2
-resource "aws_iam_role" "ec2_role" {
-  name = "ec2_app_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = { Service = "ec2.amazonaws.com" }
-    }]
-  })
+data "aws_iam_role" "lab_role" {
+  name = "LabRole"
 }
 
-resource "aws_iam_role_policy_attachment" "s3_full" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+data "aws_iam_instance_profile" "lab_profile" {
+  name = "LabRole"
 }
 
-resource "aws_iam_role_policy_attachment" "secrets_read" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
-}
-
-resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "ec2_profile"
-  role = aws_iam_role.ec2_role.name
-}
-
-# Launch Template
 resource "aws_launch_template" "app_lt" {
   name_prefix   = "app-launch-template-"
   image_id      = var.ami_id
   instance_type = "t3.micro"
-  key_name = "Library2"
+  
+  key_name      = "Library2"
 
   network_interfaces {
     associate_public_ip_address = false
@@ -40,13 +19,12 @@ resource "aws_launch_template" "app_lt" {
   }
 
   iam_instance_profile {
-    arn = aws_iam_instance_profile.ec2_profile.arn
+    arn = data.aws_iam_instance_profile.lab_profile.arn
   }
 
   user_data = base64encode(templatefile("${path.module}/user_data.sh.tpl", {}))
 }
 
-# Auto Scaling Group
 resource "aws_autoscaling_group" "asg" {
   vpc_zone_identifier = [aws_subnet.private_app_1.id, aws_subnet.private_app_2.id]
   target_group_arns   = [aws_lb_target_group.alb_tg.arn]
@@ -62,7 +40,6 @@ resource "aws_autoscaling_group" "asg" {
   }
 }
 
-# Dynamic CPU Tracking Scaling Policy
 resource "aws_autoscaling_policy" "cpu_policy" {
   name                   = "cpu-target-tracking"
   autoscaling_group_name = aws_autoscaling_group.asg.name
--- Terraform/rds.tf
+++ Terraform/rds.tf
@@ -5,7 +5,7 @@ resource "aws_secretsmanager_secret" "db_secret" {
 }
 
 resource "aws_secretsmanager_secret_version" "db_secret_val" {
-  secret_id     = aws_secretsmanager_secret.db_secret.id
+  secret_id = aws_secretsmanager_secret.db_secret.id
   secret_string = jsonencode({
     username = "admin"
     password = var.db_password
