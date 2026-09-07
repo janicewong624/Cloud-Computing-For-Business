81dec6451fb1ea38f94f71f58c8c4e7365f2e0cd
janicewong624 <janicewongyukru07@gmail.com>
2026-09-07 12:53:20 +0800

    test52

--- library-resource-scheduling/helpers.php
+++ library-resource-scheduling/helpers.php
@@ -255,7 +255,7 @@ function handle_image_upload($file, $uploadDir, $prefix = 'photo') {
     $filename = uniqid($prefix . '_', true) . '.' . $allowedMimes[$imageInfo['mime']];
 
     if (AWS_S3_BUCKET !== '') {
-        return s3_put_object($filename, file_get_contents($file['tmp_name']), $imageInfo['mime']);
+        return s3_put_object('uploads/' . $filename, file_get_contents($file['tmp_name']), $imageInfo['mime']);
     }
 
     if (!is_dir($uploadDir)) {
